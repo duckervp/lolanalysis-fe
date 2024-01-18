7eca1fde146f717fc1aef9d3864ea499cf1c8107
Trần Trung Đức <duc.trantrung99@gmail.com>
2024-01-18 14:36:00 +0700

    Revert "test"

--- src/app-config.js
+++ src/app-config.js
@@ -3,8 +3,3 @@ export const BASE_URL = "http://localhost:8080";
 export const LOL_CURRENT_VERSION = "13.24.1";
 
 export const DDRAGON_URL = "https://ddragon.leagueoflegends.com/cdn";
-
-
-// new line
-
-// new line 2
