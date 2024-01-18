5b8220122d0908ad30f1143b341db39420f88fbb
duckervp <duc.trantrung99@gmail.com>
2024-01-18 14:33:52 +0700

    test

--- src/app-config.js
+++ src/app-config.js
@@ -3,3 +3,6 @@ export const BASE_URL = "http://localhost:8080";
 export const LOL_CURRENT_VERSION = "13.24.1";
 
 export const DDRAGON_URL = "https://ddragon.leagueoflegends.com/cdn";
+
+
+// new line
